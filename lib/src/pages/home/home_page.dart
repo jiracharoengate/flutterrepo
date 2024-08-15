93eda4a4b17bfe882683199d8d58ddfc5a13d67c
user <nos.jira@gmail.com>
2024-08-15 12:06:47 +0700

    home page add listView to render title , subtitle and image from json

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -9,22 +9,52 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomepageState extends State<HomePage> {
-
   //จะถูกเรียก อัตโนมัติ เมื่อ Widget กำลังจะ render โดย สามารถจะ initial ค่าก่อนที่มันจะ render ได้
   @override
   void initState() {
     super.initState();
     WebApiService().feed();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Homepage"),
       ),
-      body: Center(
-        child: Text("1234"),
-      ),
+      //FutureBuilder คือ Widget ที่ใช้แสดงผลข้อมูลแบบ Asynchonous
+      body: Container(
+          child: FutureBuilder(
+              future: WebApiService().feed(),
+              // ค่าของ ผลลัพธ์จะอยู่ภายใต้ snapshot
+              builder: (context, snapshot) {
+                if (snapshot.hasData == false) {
+                  return const Text('Loading...');
+                }
+                final youtubes = snapshot.data;
+
+                //Better Performance
+                return ListView.builder(
+                    itemCount: youtubes!.length,
+                    itemBuilder: (context, index) {
+                      return TextButton(
+                        onPressed: () => print(youtubes[index].title),
+                        child: Card(
+                          margin: EdgeInsets.all(20.0),
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  youtubes[index].title,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(youtubes[index].subtitle),
+                                Image.network(youtubes[index].youtubeImage),
+                              ]),
+                        ),
+                      );
+                    });
+              })),
     );
   }
 }
--- lib/src/services/webapi_service.dart
+++ lib/src/services/webapi_service.dart
@@ -1,10 +1,13 @@
+import 'package:cmflutter0/src/models/youtube_response.dart';
 import 'package:dio/dio.dart';
 
 class WebApiService{
-  Future<void> feed() async {
+  Future<List<Youtube>> feed() async {
     final dio = Dio();
     // await เป็นคำสั่งว่าให้หยุดรอผลลัพธ์ก่อนแล้วค่อยรัน command บรรทัดถัดไป
     final result = await dio.get("https://codemobiles.com/adhoc/youtubes/index_new.php?username=admin&password=password&type=songs");
-    print(result.data);
+    final youtubeResponse = youtubeResponseFromJson(result.data);
+    return youtubeResponse.youtubes;
+
   }
 }
